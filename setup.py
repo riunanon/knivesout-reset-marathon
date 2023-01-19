bb38c367758e1e78f328d60b9c356f8d5278ed5d
りう <123007508+riunanon@users.noreply.github.com>
2023-01-19 20:17:45 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/riunanon/knivesout-reset-marathon-shortcut-keyboard-pythonista3",
-    packages=setuptools.find_packages(),
     packages=[
         "knivesout",
     ],
