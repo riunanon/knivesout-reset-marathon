b5c287e52ee874f562417c173cc6a30c15049bf3
りう <123007508+riunanon@users.noreply.github.com>
2023-01-19 18:59:22 +0900

    Create example.py

---
+++ example.py
@@ -0,0 +1,7 @@
+import knivesout
+
+knivesout.random_digits_name(8) #数字のみのランダム文字列　括弧内に文字数を入力(int)
+knivesout.random_letters_name(8) #アルファベットのみのランダム文字列　括弧内に文字数を入力(int)
+knivesout.random_name(8) #数字とアルファベット混合のランダム文字列　括弧内に文字数を入力(int)
+knivesout.hai() #「はい」を入力
+knivesout.passcode('passcode') #パスコードを入力　括弧内にパスコードを入力(str)
