7d7da4776c1c113531a1dc1a4034bb454825c4cc
りう <123007508+riunanon@users.noreply.github.com>
2023-01-19 20:15:55 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/riunanon/knivesout-reset-marathon-shortcut-keyboard-pythonista3",
     packages=setuptools.find_packages(),
+    packages=[
+        "knivesout",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
