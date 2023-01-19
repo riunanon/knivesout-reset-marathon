f7e888d14cdcfe5b3d3b8a1f66b528f40dc464cf
りう <123007508+riunanon@users.noreply.github.com>
2023-01-19 19:02:26 +0900

    Update knivesout.py

--- knivesout.py
+++ knivesout.py
@@ -2,25 +2,30 @@
 
 import keyboard,random,clipboard,string
 
-def random_digits_name(n): #数字のみのランダム文字列
+def random_digits_name(n,enter=True): #数字のみのランダム文字列
     randlst = [random.choice(string.digits) for i in range(n)]
     keyboard.insert_text(''.join(randlst))
-    keyboard.insert_text('\n')
+    if enter:
+      keyboard.insert_text('\n')
 
-def random_letters_name(n): #アルファベットのみのランダム文字列
+def random_letters_name(n,enter=True): #アルファベットのみのランダム文字列
     randlst = [random.choice(string.ascii_letters) for i in range(n)]
     keyboard.insert_text(''.join(randlst))
-    keyboard.insert_text('\n')
+    if enter:
+      keyboard.insert_text('\n')
 
-def random_name(n): #数字とアルファベット混合のランダム文字列
+def random_name(n,enter=True): #数字とアルファベット混合のランダム文字列
     randlst = [random.choice(string.digits+string.ascii_letters) for i in range(n)]
     keyboard.insert_text(''.join(randlst))
-    keyboard.insert_text('\n')
+    if enter:
+      keyboard.insert_text('\n')
 
-def hai(): #「はい」を入力
+def hai(enter=True): #「はい」を入力
     keyboard.insert_text('はい')
-    keyboard.insert_text('\n')
+    if enter:
+      keyboard.insert_text('\n')
 
-def passcode(passcode): #パスコードを入力
+def passcode(passcode,enter=True): #パスコードを入力
     keyboard.insert_text(passcode)
-    keyboard.insert_text('\n')
+    if enter:
+      keyboard.insert_text('\n')
